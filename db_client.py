b02ed93d1f911753256ffe4eaf9dfabe4afbac9b
aakriti-malla-engg-dev <aakriti.malla@moengage.com>
2023-01-24 19:22:57 +0530

    update

--- db_client.py
+++ db_client.py
@@ -1,26 +1,45 @@
+import os
 import rethinkdb as r
-from rethinkdb.errors import RqlRuntimeError
+from rethinkdb.errors import RqlRuntimeError, RqlDriverError
+rdb = r.RethinkDB()
 
 RDB_HOST = 'localhost'
-RDB_PORT = 29015
+RDB_PORT = 28015
 
 PROJECT_DB = 'todo'
 PROJECT_TABLE = 'notes'
 
-rdb = r.RethinkDB()
 db_connection = rdb.connect(RDB_HOST, RDB_PORT)
 
 
 def dbSetup():
     try:
-        r.db_create(PROJECT_DB).run(db_connection)
-        print("Database setup completed.")
+        rdb.db_create(PROJECT_DB).run(db_connection)
+        print('Database setup completed.')
     except RqlRuntimeError:
         try:
-            r.db(PROJECT_DB).table_create(PROJECT_TABLE).run(db_connection)
-            print("Table creation completed")
+            rdb.db(PROJECT_DB).table_create(PROJECT_TABLE).run(db_connection)
+            print('Table creation completed')
         except:
-            print("Table already exists.Nothing to do")
+            print('Table already exists.Nothing to do')
 
 
 dbSetup()
+
+# import rethinkdb as r
+# from rethinkdb.errors import RqlRuntimeError
+#
+# rdb = r.RethinkDB()
+# PROJECT_DB = 'todo'
+# PROJECT_TABLE = 'notes'
+# if __name__ == "__main__":
+#     conn = rdb.connect(host='127.0.0.1', port='28015',db=PROJECT_DB)
+#     try:
+#         cursor = rdb.table(PROJECT_TABLE).run(conn)
+#         for record in cursor:
+#             print(record)
+#     except RqlRuntimeError as err:
+#         print(err.message)
+#     finally:
+#         conn.close()
+
